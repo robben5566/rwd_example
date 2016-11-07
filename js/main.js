62424e290e45b639e8da464cf5df2118c3c051cd
robben5566 <ccav31104@gmail.com>
2016-11-07 15:49:00 +0800

    0003

---
+++ js/main.js
@@ -0,0 +1,43 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+        $(".header-area").sticky({topSpacing: 0});
+
+        $('li.smooth-menu a').bind('click', function(event) {
+            var $anchor = $(this);
+            var headerH = '64';
+            $('html, body').stop().animate({
+                scrollTop : $($anchor.attr('href')).offset().top - headerH + "px"
+            }, 1200, 'easeInOutExpo');
+            
+            event.preventDefault();
+        });
+
+       
+        
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 95
+        });
+        
+        $('.parallax-bg, .top-area-bg').scrolly({bgParallax: true});
+        
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
